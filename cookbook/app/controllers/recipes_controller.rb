781331c7e23fe2d0449c3a102659770af90389bf
“Ian <ianyhkwok@hotmail.com>
2015-10-29 01:41:11 +0800

    recipe index linked

--- cookbook/app/controllers/recipes_controller.rb
+++ cookbook/app/controllers/recipes_controller.rb
@@ -1,75 +1,42 @@
 class RecipesController < ApplicationController
- #index
-  get '/recipe' do
-    @recipes = Recipe.all
-  end
-  #show individual
-  get '/recipe/view/:id' do
-    @recipe = Recipe.find(params[:id])
-    erb(:"recipes/view")
-  end
-  #edit page
-  get '/recipe/edit/:id' do
-    @recipe = Recipe.find(params[:id])
-    @courses = Course.all
-    erb(:"recipes/edit")
-  end
-  #new
-  get '/recipe/add' do
-    @courses = Course.all
-    erb(:"recipes/add")
-  end
-  #update
-  put '/recipe/edit/:id' do
-    @recipe = Recipe.find(params[:id])
-    @recipe.update_attributes(params[:recipe])
-    @ingredients = params[:ingredients].split
-    @course = Course.find_by title: params[:course]
-    @recipe.course = @course
-    @recipe.save
+  before_action :set_course
 
-    @recipe.ingredients = []
+  def index
+    @recipes = @course.recipes
+  end
 
-    @ingredients.each do |ingredient|
-      @cap_ingredient = ingredient.capitalize
-      @search_ingredient = Ingredient.find_by name:@cap_ingredient
-      if !@search_ingredient
-        add_ingredient = Ingredient.create(name: @cap_ingredient)
-        @recipe.ingredients << add_ingredient
-      else
-        @recipe.ingredients << @search_ingredient
-      end
+  def show
+    @recipe = @course.recipes.find(params[:id])
+  end
+
+  def new
+    @recipe = @course.recipes.new
+  end
+
+  def create
+    @recipe = @course.recipes.new(recipe_params)
+    if @recipe.save
+      redirect_to course_recipe_url
     end
-    redirect("/recipe/")
   end
-  #
-  #post
-  post '/recipe/add' do
-    @ingredients = params[:ingredients].split
-    @recipe = Recipe.create(params[:recipe])
-    @course = Course.find_by title: params[:course]
-    @recipe.course = @course
-    @recipe.save
 
-
-    @ingredients.each do |ingredient|
-      @cap_ingredient = ingredient.capitalize
-      @search_ingredient = Ingredient.find_by name:@cap_ingredient
-      if !@search_ingredient
-        add_ingredient = Ingredient.create(name: @cap_ingredient)
-        @recipe.ingredients << add_ingredient
-      else
-        @recipe.ingredients << @search_ingredient
-      end
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to course_recipe_path
     end
-    redirect("/recipe/")
   end
 
-  #destroy
-  delete '/recipe/:id' do
-    Recipe.delete(params[:id])
-    redirect("/recipe/")
+  def destroy
+    @recipe.destroy
+    redirect_to course_recipe_path(@recipe.course_id)
   end
-end
 
+  private
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  def recipe_params
+      params.require(:recipe).permit(:name, :instruction, :serving, :course_id)
+    end
 end
